2ceed199fe322ae11e77b09e32215c32bddd63e4
retornam <retornam@users.noreply.github.com>
2022-05-31 17:28:06 -0700

    add docker files
    local files to build a Ghidra server docker image locally.

---
+++ docker-local/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$1" = 'server' ]; then
+  shift
+  # Figure out public address
+  export GHIDRA_PUBLIC_HOSTNAME=${GHIDRA_PUBLIC_HOSTNAME:-$(dig +short myip.opendns.com @resolver1.opendns.com)}
+
+  # Add users
+  GHIDRA_USERS=${GHIDRA_USERS:-admin}
+  if [ ! -e "/repos/users" ] && [ ! -z "${GHIDRA_USERS}" ]; then
+    mkdir -p /repos/~admin
+    for user in ${GHIDRA_USERS}; do
+      echo "Adding user: ${user}"
+      echo "-add ${user}" >> /repos/~admin/adm.cmd
+    done
+  fi
+
+  exec "/ghidra/server/ghidraSvr" console
+fi
+
+exec "$@"
